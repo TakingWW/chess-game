b390c82f0ce77ca212d5242797ac7a489fa01d01
TakingWW <jub_camara@hotmail.com>
2023-09-27 10:15:15 -0400

    Update build system to compile the new module

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-javac -d target -sourcepath . src/main/java/com/joao/Main.java src/main/java/com/joao/objects/*.java
+javac -d target -sourcepath . src/main/java/com/joao/Main.java src/main/java/com/joao/objects/*.java src/main/java/com/joao/net/*.java
 if [ $? == 0 ]
 then
-   java -cp target/ com.joao.Main
+    java -cp target/ com.joao.Main
 fi
